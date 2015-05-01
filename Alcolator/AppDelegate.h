dd9f0aac8298cc8adbc01d1340caced0f5890368
dothework <marktc@gmail.com>
2015-05-01 12:00:31 -0400

    Storyboarding Alcolator

---
+++ Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by DAD on 5/1/15.
+//  Copyright (c) 2015 Mark Carpenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by DAD on 5/1/15.
+//  Copyright (c) 2015 Mark Carpenter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
