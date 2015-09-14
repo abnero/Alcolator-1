fa4942a9d657bfa21e2eff9e2aadf979fcbbbaa6
abnero <abnero@gmail.com>
2015-09-14 09:09:18 -0700

    Checkpoint 20 completed

---
+++ WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Abner Flores on 9/14/15.
+//  Copyright (c) 2015 Mark Carpenter. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
