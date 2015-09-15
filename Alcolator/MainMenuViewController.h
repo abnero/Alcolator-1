c8c45fa1ac550e6fc76e1e960e425091ceef1787
abnero <abnero@gmail.com>
2015-09-15 08:47:32 -0700

    Checkpoint 21 Completed

---
+++ Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Abner Flores on 9/15/15.
+//  Copyright (c) 2015 Mark Carpenter. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
